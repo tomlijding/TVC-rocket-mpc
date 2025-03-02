e4c9673c2d6946c35cd6fa0f2c58fc50b7cbecc2
Tom <tomlijding@gmail.com>
2025-03-02 18:48:51 +0100

    Added: - main.m: Added a couple of line to define the linearized matrice

--- main.m
+++ main.m
@@ -10,3 +10,11 @@ addpath Functions
 variables = [phi, g, m, J_t,d, S, l,Sm,C_A, C_Y_beta,C_N_alpha, C_m_q, C_m_alpha, C_n_r, C_n_beta];
 
 sysDynamics = initSymbolicSysDynamics(phi, g, m, J_t,d, S, l,Sm,C_A, C_Y_beta,C_N_alpha, C_m_q, C_m_alpha, C_n_r, C_n_beta,rho);
+
+syms theta Psi v w u p q r mu1 mu2 T
+
+linearizedDynamics = jacobian(sysDynamics,[theta,Psi,v,w,u,p,q,r,mu1,mu2,T]);
+
+LTI.A = linearizedDynamics(:,1:8);
+LTI.B = linearizedDynamics(:,9:11);
+
