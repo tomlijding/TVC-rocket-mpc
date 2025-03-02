5b29fded22016cd7364600999690810e269da4db
Tom <tomlijding@gmail.com>
2025-03-02 18:45:05 +0100

    Added: - initSymbolicSysDynamics: Function which defines symbolic system dynamics - defineConstants: Function which defines constants - main.m: Main file to run

---
+++ Functions/defineConstants.m
@@ -0,0 +1,21 @@
+function [phi, g, m, J_t,d, S, l,Sm,C_A, C_Y_beta,C_N_alpha, C_m_q, C_m_alpha, C_n_r, C_n_beta, rho] = defineConstants
+%DEFINECONSTANTS Summary of this function goes here
+%   Detailed explanation goes here
+phi = 1;
+g = 2;
+m = 3;
+J_t = 4;
+d = 14;
+S = 5;
+l = 6;
+Sm = 7;
+C_A = 8;
+C_Y_beta = 9;
+C_N_alpha = 10;
+C_m_q = 11;
+C_m_alpha = 12;
+C_n_r = 13;
+C_n_beta = 14;
+rho = 16;
+end
+
---
+++ main.m
@@ -0,0 +1,12 @@
+clear all
+close all
+clc
+addpath Functions
+
+%% 
+
+[phi, g, m, J_t,d, S, l,Sm,C_A, C_Y_beta,C_N_alpha, C_m_q, C_m_alpha, C_n_r, C_n_beta,rho] = defineConstants;
+
+variables = [phi, g, m, J_t,d, S, l,Sm,C_A, C_Y_beta,C_N_alpha, C_m_q, C_m_alpha, C_n_r, C_n_beta];
+
+sysDynamics = initSymbolicSysDynamics(phi, g, m, J_t,d, S, l,Sm,C_A, C_Y_beta,C_N_alpha, C_m_q, C_m_alpha, C_n_r, C_n_beta,rho);
