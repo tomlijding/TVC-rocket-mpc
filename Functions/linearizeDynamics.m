4b9a7523d53d3e3c6a892c1e692d0a575d376756
Tom <tomlijding@gmail.com>
2025-03-03 14:32:21 +0100

    Added function

---
+++ Functions/linearizeDynamics.m
@@ -0,0 +1,14 @@
+function LTI = linearizeDynamics(symbolicDynamics,symbolicStates,symbolicInputs,symbolicDisturbances,stateOperatingPoints,inputOperatingPoints,disturbancOperatingPoints)
+%linearizeDynamics Linearize the dynamics of the vector symbolicDynamics
+%w.r.t. the vector symbolic states, inputs and disturbances at the
+%specified operating points
+%   Detailed explanation goes here
+symbolicVector = [symbolicStates;symbolicInputs;symbolicDisturbances];
+operatingPoints = [stateOperatingPoints;inputOperatingPoints;disturbancOperatingPoints];
+symbolicA = jacobian(symbolicDynamics,symbolicStates);
+symbolicB = jacobian(symbolicDynamics,symbolicInputs);
+symbolicG = jacobian(symbolicDynamics,symbolicDisturbances);
+LTI.A = subs(symbolicA,symbolicVector,operatingPoints);
+LTI.B = subs(symbolicB,symbolicVector,operatingPoints);
+LTI.G = subs(symbolicG,symbolicVector,operatingPoints);
+end
